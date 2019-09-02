fa9415c3389c2122b96a3d85509b64facc3c84c0
Igor D <garik_1@mail.ru>
2019-09-03 00:01:57 +0300

    первая контрольная точка

---
+++ script.js
@@ -0,0 +1,59 @@
+"use strict"
+
+
+var number = 5;
+var string = "Hello!";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(4/0);
+console.log("правда"/2);
+let some;
+console.log(some);
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+
+console.log(persone.name);
+console.log(persone['age']);
+
+let arr = ["apple.png","carrot.jpg","melon.bmp"];
+console.log(arr[2]);
+
+// console.log(typeof(arr));
+// alert("Hello World!");
+//let answer = confirm("Are you here?");
+// let answer = prompt("Are you over 18?", "YES");
+
+
+//console.log(typeof(arr));
+//console.log(typeof(22 + 33));
+
+let incr = 10,
+    decr = 10;
+
+//incr++;
+//decr--;
+
+console.log(incr++);
+console.log(decr--);
+
+//console.log("3" === 3);
+
+let isChecked = true,
+    isClose = false;
+
+console.log(isChecked && isClose);
+console.log(isChecked || isClose);
+
+
+
+
+
